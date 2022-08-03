ecc1392bcbd25399961e670ed95d7b82b229a40f
Ali Babolhavaeji <ababolha@cccis.com>
2022-08-03 12:24:39 -0500

    Bootstrap flex container is added.

--- tic_tok_toe_game/src/App.js
+++ tic_tok_toe_game/src/App.js
@@ -1,23 +1,48 @@
 import React from "react";
+import { MDBBtn } from "mdb-react-ui-kit";
+
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "./styles.css";
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
+      <MDBBtn className={this.props.className} onClick={() => this.props.onClick()}>
         {this.props.value}
-      </button>
+      </MDBBtn>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSqure(i , style) {
+    return <Square className={style} value={i} onClick={() => alert(i)} />;
+  }
+
+  render() {
+    let squares = [];
+    let posts = [];
+    for (let i = 0; i < 20; i++) {
+      squares.push(this.renderSqure(i, "Squre"));
+    }
+
+    for (let i = 0; i < 20; i++) {
+        posts.push(this.renderSqure(i, "Squre post"));
+      }
+
+    return (
+      <div className="mainContainer">
+        <div className="containerBlock leftContainer">{squares}</div>
+        <div className="containerBlock middleContainer">{posts}</div>
+        <div className="containerBlock rightContainer">{squares}</div>
+      </div>
     );
   }
 }
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <div>In the App</div>
-        <Square value="X" onClick={() => alert("X")} />
-      </div>
-    );
+    return <Board />;
   }
 }
 
